7a6beae953812c82860ead2c91bffa44cc93e1e4
EDER LEITE <ederaparecidoleite@hotmail.com>
2022-02-23 11:11:13 -0300

    Faltou terminar o produto

---
+++ orientacao_objeto/Controllers/ProdutoController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using orientacao_objeto.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace orientacao_objeto.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        public static List<Produtos> lsProduto = new List<Produtos>();
+        public IActionResult Index()
+        {
+            return View(lsProduto);
+        }
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Salvar (Produtos item)
+        {
+            lsProduto.Add(item);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ orientacao_objeto/Models/Produtos.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace orientacao_objeto.Models
+{
+    public class Produtos
+    {
+        public string produto { get; set; }
+        public int ID { get; set; }
+        public string descricao { get; set; }
+        public int valor { get; set; }
+    }
+}
