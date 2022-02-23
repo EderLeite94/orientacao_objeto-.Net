e96a5ce0cc883e7cb4809dd89ae8f58b326665b6
0220832111016 <0220832111016@LAB4TC05>
2022-02-23 09:54:48 -0300

    Adicionar arquivos de projeto.

---
+++ orientacao_objeto/Controllers/PessoaController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using orientacao_objeto.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace orientacao_objeto.Controllers
+{
+    
+    public class PessoaController : Controller
+    {
+        public static List<Pessoa> lsPessoa = new List<Pessoa>();
+
+        public IActionResult Index()
+        {
+            return View(lsPessoa);
+        }
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Salvar(Pessoa item)                                 
+        {
+            lsPessoa.Add(item);
+            return RedirectToAction("Index");
+        }
+    }
+}
